03c26e7f76797f7248ce4ad83fc58697a53adf07
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-04-12 08:10:29 -0400

    Fix confirmation URL in mailer

--- lib/generators/active_registration/templates/confirmation_mailer.rb
+++ lib/generators/active_registration/templates/confirmation_mailer.rb
@@ -1,7 +1,7 @@
 class ConfirmationMailer < ApplicationMailer
   def confirmation_instructions(user)
     @user = user
-    @confirmation_url = registration_confirm_url(token: @user.confirmation_token)
+    @confirmation_url = confirm_registration_url(token: @user.confirmation_token)
     mail(to: @user.email_address, subject: "Confirm Your Email")
   end
 end
